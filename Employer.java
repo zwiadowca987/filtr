746be94ce71f0313daeead4b32008baf268bb783
zwiadowca987 <32812930+zwiadowca987@users.noreply.github.com>
2019-03-13 23:16:04 +0100

    Add files via upload

---
+++ Employer.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Employer {
+    private String name;
+    private int salary;
+
+    public Employer(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Collection;
+import java.util.LinkedList;
+
+public class Main {
+    private static void filtr(Collection<Employer> c) {
+        for(int i = 0; i < c.size(); i++) {
+            Employer emp = new Employer(c.);//ERROR
+            if(emp.getSalary() <= 4000) System.out.println(emp);
+        }
+    }
+
+    public static void main(String[] args) {
+        Collection<Employer> collOfEmp = new LinkedList<>();
+
+        collOfEmp.add(new Employer("Jan Kowalski", 4000));
+        collOfEmp.add(new Employer("Monika Kwiatkowska", 3000));
+        collOfEmp.add(new Employer("Janusz Drygała", 400));
+        collOfEmp.add(new Employer("Krzysztof Nowak", 5000));
+        collOfEmp.add(new Employer("Rafał Komorniczkowski", 40000));
+
+        filtr(collOfEmp);
+    }
+}
